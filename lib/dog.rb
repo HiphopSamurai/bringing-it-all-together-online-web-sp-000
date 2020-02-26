9af855c7a1ffe88274754f5bdd655f8a04e7aa70
Garrett Thompson <gtcruz.gt@gmail.com>
2020-02-26 05:31:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -91,7 +91,12 @@ class Dog
   end
 
   def update
-    ###
+    sql = <<-SQL
+      UPDATE dogs SET name = ?, breed = ?, WHERE id = ?
+      SQL
+
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
   end
 
 end
