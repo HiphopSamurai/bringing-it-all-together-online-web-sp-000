e83587e94c5a00e45799451a29b470e71200983f
Garrett Thompson <gtcruz.gt@gmail.com>
2020-02-26 05:29:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -80,8 +80,14 @@ class Dog
     new_dog
   end
 
-  def self.find_by_name
-    ###
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM dogs WHERE name = ?
+      SQL
+
+    DB[:CONN].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def update
